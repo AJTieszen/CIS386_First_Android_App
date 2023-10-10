df0f0b24e5acd2c6c5753a71d7809e987e131183
Andrew Tieszen <pilottzn@gmail.com>
2023-10-09 20:23:55 -0600

    Added toast message and counter

--- app/src/main/java/com/example/cis386_first_android_app/FirstFragment.kt
+++ app/src/main/java/com/example/cis386_first_android_app/FirstFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.cis386_first_android_app.databinding.FragmentFirstBinding
 
@@ -22,7 +24,7 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
@@ -32,9 +34,26 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+        binding.randomButton.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+        binding.toastButton.setOnClickListener {
+            val myToast = Toast.makeText(context, getString(R.string.hello_toast), Toast.LENGTH_SHORT)
+            myToast.show()
+        }
+        binding.countButton.setOnClickListener {
+            countMe(view)
+        }
+    }
+
+    private fun countMe(view: View) {
+        val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+        val countString = showCountTextView.text.toString()
+
+        var count = countString.toInt()
+        count ++
+
+        showCountTextView.text = count.toString()
     }
 
     override fun onDestroyView() {
