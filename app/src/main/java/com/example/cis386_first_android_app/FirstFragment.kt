531191f972bb212459a4842f050e19ce0abcc8b3
Andrew Tieszen <pilottzn@gmail.com>
2023-10-30 20:01:11 -0600

    Built second fragment and added safe args. Passed arguments between fragments.

--- app/src/main/java/com/example/cis386_first_android_app/FirstFragment.kt
+++ app/src/main/java/com/example/cis386_first_android_app/FirstFragment.kt
@@ -35,8 +35,13 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.randomButton.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+            val currentCount = showCountTextView.text.toString().toInt()
+
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(currentCount)
+            findNavController().navigate(action)
         }
+
         binding.toastButton.setOnClickListener {
             val myToast = Toast.makeText(context, getString(R.string.hello_toast), Toast.LENGTH_SHORT)
             myToast.show()
--- app/src/main/java/com/example/cis386_first_android_app/SecondFragment.kt
+++ app/src/main/java/com/example/cis386_first_android_app/SecondFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.example.cis386_first_android_app.databinding.FragmentSecondBinding
+import androidx.navigation.fragment.navArgs
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -29,12 +31,25 @@ class SecondFragment : Fragment() {
 
     }
 
+    val args:SecondFragmentArgs by navArgs()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
+
+        val count = args.myArg
+        val countText = "Here is a random number between 0 and $count."
+        view.findViewById<TextView>(R.id.textView_header).text = countText
+
+        var random = java.util.Random()
+        var randomNumber = 0
+
+        if (count > 0) {
+            randomNumber = random.nextInt(count + 1)
+        }
+        view.findViewById<TextView>(R.id.textview_second).text = randomNumber.toString()
     }
 
     override fun onDestroyView() {
